e4779311b2d6b5c2f7dd65f6732a815c6af33a4c
Christophe Meneboeuf <christophe@xtof.info>
2017-11-09 10:45:54 +0100

    License

--- src/CFactoryHashes.hpp
+++ src/CFactoryHashes.hpp
@@ -1,11 +1,19 @@
-
-//
-//  CFactoryHashes.hpp
-//  compare_folder
-//
-//  Created by Christophe Meneboeuf on 24/10/2017.
-//
-//
+/*
+3  *  Copyright (C) 2017 Christophe Meneboeuf <christophe@xtof.info>
+4  *
+5  *  This program is free software: you can redistribute it and/or modify
+6  *  it under the terms of the GNU General Public License as published by
+7  *  the Free Software Foundation, either version 3 of the License, or
+8  *  (at your option) any later version.
+9  *
+10  *  This program is distributed in the hope that it will be useful,
+11  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+12  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+13  *  GNU General Public License for more details.
+14  *
+15  *  You should have received a copy of the GNU General Public License
+16  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+17 */
 
 #ifndef _SRC_CFactoryHashes_hpp__
 #define _SRC_CFactoryHashes_hpp__
