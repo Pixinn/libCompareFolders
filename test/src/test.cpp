94564edafe6998ccffee98f349f1b91cd978dea0
Christophe Meneboeuf <christophe@xtof.info>
2017-11-22 00:55:05 +0100

    Creating random files

---
+++ test/src/test.cpp
@@ -0,0 +1,123 @@
+/*
+3  *  Copyright (C) 2017 Christophe Meneboeuf <christophe@xtof.info>
+4  *
+5  *  This program is free software: you can redistribute it and/or modify
+6  *  it under the terms of the GNU General Public License as published by
+7  *  the Free Software Foundation, either version 3 of the License, or
+8  *  (at your option) any later version.
+9  *
+10  *  This program is distributed in the hope that it will be useful,
+11  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+12  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+13  *  GNU General Public License for more details.
+14  *
+15  *  You should have received a copy of the GNU General Public License
+16  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+17 */
+
+#include <cstdlib>
+#include <cstdint>
+#include <stdexcept>
+#include <list>
+#include <array>
+#include <boost\filesystem.hpp>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+namespace fs = boost::filesystem;
+using namespace std;
+
+/// @brief Returns a random string
+string Random_String(const unsigned length)
+{
+    const auto len = (1 + length) & 0xFF;
+    constexpr auto size_charset = 66u;
+    constexpr array<char, size_charset> charset = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', \
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    auto buffer = new char[len+1];
+    for (auto i = 0u; i < len; ++i) {
+        buffer[i] = charset[rand() % size_charset];
+    }
+    buffer[len] = 0u;
+    string str{ buffer };
+    delete[] buffer;
+    return str;
+}
+
+/// @brief Creates a random named folder
+/// @param parent Where to create the folder.
+fs::path Create_Random_Folder(const fs::path parent)
+{
+    constexpr uint8_t max_len_name = 20u;
+    const auto name = parent / Random_String(rand() % max_len_name);
+    if (!fs::exists(name) && !fs::create_directory(name)) {
+        throw runtime_error{ "Cannot create folder " + name.string() };
+    }
+    return name;
+}
+
+/// @brief  Creates random files inside the provided folder
+///         Returns the paths of all created files.
+/// @param  folder Folder to create the files in.
+list<fs::path> Create_Random_Files(const fs::path folder)
+{
+    list<fs::path> paths;
+    constexpr uint8_t max_len_filename = 20u;
+    constexpr uint8_t max_nb_files = 100u;
+
+    const unsigned nb_files = rand() % max_nb_files;
+    for (auto i = 0u; i < nb_files; ++i)
+    {
+        auto filepath = folder / Random_String(rand() % max_len_filename);
+        while (fs::exists(filepath)) {
+            filepath = folder / Random_String(rand() % max_len_filename);
+        }
+        fs::ofstream stream{ filepath, fs::ofstream::binary };
+        constexpr auto max_size_file = 65535u;
+        const auto size_file = rand() % max_size_file;
+        auto buffer = new char[size_file];
+        for (auto j = 0u; j < size_file; ++j) {
+            buffer[j] = rand() % 0xff;
+        }
+        stream.write(buffer, size_file);
+    }
+
+    return paths;
+}
+
+/// @brief Builds a file tree to perform the tests on
+///        Returns the files that were created
+list<fs::path> Build_Test_Files()
+{
+    list<fs::path> files;
+
+    const auto folder_tmp = fs::temp_directory_path();
+    const auto folder_left = folder_tmp / "left";
+    const auto folder_right = folder_tmp / "right";
+    if (!fs::exists(folder_left) && !fs::create_directory(folder_left)) {
+        throw runtime_error{ "Cannot create folder " + folder_left.string() };
+    }
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    constexpr uint8_t max_nb_folders = 10u;
+    const unsigned nb_folders = rand() % max_nb_folders;
+    for (auto i = 0u; i < nb_folders; ++i) {
+        const auto folder = Create_Random_Folder(folder_left);
+        files.merge(Create_Random_Files(folder));
+    }
+
+    return files;
+    
+}
+
+
+TEST_CASE("NOMINAL")
+{
+    const auto files = Build_Test_Files();
+    for (const auto& file : files) {
+        fs::remove(file);
+    }
+}
